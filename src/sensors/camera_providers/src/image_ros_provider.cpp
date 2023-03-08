1844cb06f31fe1727883871e1b0537141979909a
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2023-03-08 16:25:41 +0100

    rudimentary msg callbacks

--- src/sensors/camera_providers/src/image_ros_provider.cpp
+++ src/sensors/camera_providers/src/image_ros_provider.cpp
@@ -1,4 +1,5 @@
 #include "sensors/camera_providers/image_ros_provider.hpp"
+#include <cv_bridge/cv_bridge.h>
 
 #include <filesystem>
 #include <fstream>
@@ -29,7 +30,47 @@ void ImageRosProvider::ImageReadCallback(const sensor_msgs::Image& img_msg) {
     cv_ptr = cv_bridge::toCvCopy(img_msg, sensor_msgs::image_encodings::MONO8);
   } catch (cv_bridge::Exception& e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
+    return; // Or handle the error in some other way
+  }
+
+  // Access the converted OpenCV image
+  cv::Mat img = cv_ptr->image;
+  Precision exposure_time = 1;
+  
+  uint64_t timestamp = img_msg->header.stamp.toNSec();
+  
+  // feed the cv::Mat to the CameraDataFrame
+  auto frame = std::make_unique<CameraDataFrame>(img_msg->header.frame_id, img, exposure_time, timestamp )
+
+  frame_batch_.push_back(frame)
+}
+
+void ImageRosProvider::ImageReadCallback(const sensor_msgs::Image& img_msg) {
+  ROS_INFO("I heard: frame_id [%s]", img_msg->header.frame_id);
+  ROS_INFO("I heard: timestamp [%s]", img_msg->header.stamp);
+  // use cv_bridge to read data from sensor_msgs:Image& img_msg to cv::Mat
+  // Assuming img_msg is the ROS image message to be converted
+  cv_bridge::CvImagePtr cv_ptr;
+
+  try {
+    cv_ptr = cv_bridge::toCvCopy(img_msg, sensor_msgs::image_encodings::MONO8);
+  } catch (cv_bridge::Exception& e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
     return;  // Or handle the error in some other way
+  /**
+   * @brief [TODO:summary]
+   *
+   */
+  ros::NodeHandle nh;
+
+  subscriber_ = nh.subscribe(topic_, 1, &ImageRosProvider::TopicCallback, this);
+}
+
+std::unique_ptr<CameraDataFrame> ImageRosProvider::nextFrame() {}
+
+
+size_t ImageRosProvider::queueSize() {
+    return std::numeric_limits<size_t>::max();
   }
 
   // Access the converted OpenCV image
@@ -87,3 +128,77 @@ size_t ImageRosProvider::queueSize() { return std::numeric_limits<size_t>::max()
 }  // namespace providers
 }  // namespace sensors
 }  // namespace dsopp
+
+
+class ImageRosProvider : public CameraProvider {
+ public:
+  /**
+   * Creating a provider that reads images from a ROS topic.
+   *
+   * @param nh ROS node handle
+   * @param topic_name Name of the ROS topic from which images are loaded.
+   * @param batch_size Frame batch size
+   * @param convert_to_grayscale ``true`` if conversion to grayscale is needed
+   */
+  ImageRosProvider(ros::NodeHandle &nh, const std::string &topic_name, size_t batch_size, bool convert_to_grayscale = false)
+      : batch_size_(batch_size), convert_to_grayscale_(convert_to_grayscale) {
+    subscriber_ = nh.subscribe(topic_name, batch_size_, &ImageRosProvider::imageCallback, this);
+  }
+
+  /**
+   * method to fetch images from the ROS topic.
+   *
+   * @return next image from the ROS topic
+   */
+  std::unique_ptr<CameraDataFrame> nextFrame() override {
+    std::unique_ptr<CameraDataFrame> frame = nullptr;
+    if (!frame_batch_.empty()) {
+      frame = std::move(frame_batch_.front());
+      frame_batch_.pop_front();
+    }
+    return frame;
+  }
+
+  /**
+   * method to access queue size.
+   *
+   * @return number of frames in the ROS topic that have not been processed yet.
+   */
+  size_t queueSize() override {
+    return frame_batch_.size();
+  }
+
+  ~ImageRosProvider() override = default;
+
+ private:
+  /**
+   * Callback function for handling incoming ROS messages
+   * @param image_msg ROS message containing the image data
+   */
+  void imageCallback(const sensor_msgs::Image::ConstPtr& image_msg) {
+    // Create a CameraDataFrame object and populate it with data from the ROS message
+    auto frame = std::make_unique<CameraDataFrame>();
+    frame->image_data = image_msg->data;
+    frame->image_width = image_msg->width;
+    frame->image_height = image_msg->height;
+    frame->image_encoding = image_msg->encoding;
+    frame->timestamp_ns = image_msg->header.stamp.toNSec();
+
+    // Convert to grayscale if needed
+    if (convert_to_grayscale_) {
+      // TODO: implement grayscale conversion
+    }
+
+    // Add the frame to the frame batch
+    frame_batch_.push_back(std::move(frame));
+  }
+
+  /** Size of the batch */
+  size_t batch_size_;
+  /** Flag indicating whether conversion to grayscale is needed */
+  bool convert_to_grayscale_;
+  /** ROS subscriber for the image topic */
+  ros::Subscriber subscriber_;
+  /** Container containing camera frames */
+  std::deque<std::unique_ptr<CameraDataFrame>> frame_batch_;
+};
